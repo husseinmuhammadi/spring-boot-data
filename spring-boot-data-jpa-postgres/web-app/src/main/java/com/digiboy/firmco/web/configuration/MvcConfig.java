e44cc4eafee6f0b6f355afc33ed27e7e2fb0db9f
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-03-16 09:06:20 +0330

    firm-co

---
+++ spring-boot-data-jpa-postgres/web-app/src/main/java/com/digiboy/firmco/web/configuration/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.digiboy.firmco.web.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/access-denied").setViewName("error/403");
+    }
+}
