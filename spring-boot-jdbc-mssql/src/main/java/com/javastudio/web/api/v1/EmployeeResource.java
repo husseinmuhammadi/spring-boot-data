5bc14b31c20add66ab79907f5b4dad03eabb9448
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-04-22 10:26:07 +0430

    Find employee by code

--- spring-boot-jdbc-mssql/src/main/java/com/javastudio/web/api/v1/EmployeeResource.java
+++ spring-boot-jdbc-mssql/src/main/java/com/javastudio/web/api/v1/EmployeeResource.java
@@ -2,22 +2,27 @@ package com.javastudio.web.api.v1;
 
 import com.javastudio.api.EmployeeService;
 import com.javastudio.dto.Employee;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeResource {
 
     @Autowired
+    private Logger logger;
+
+    @Autowired
     private EmployeeService employeeService;
 
-
     @GetMapping
-    public ResponseEntity<List<Employee>> findAll() {
+    public ResponseEntity<List<Employee>> findAll(@RequestParam("code") Optional<String> code) {
+        logger.info("code: {}", code);
         return ResponseEntity.ok(employeeService.findAll());
     }
 
@@ -25,10 +30,4 @@ public class EmployeeResource {
     public ResponseEntity<Employee> findById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(employeeService.findById(id));
     }
-
-    @GetMapping
-    public ResponseEntity<Employee> findByCode(@RequestParam("code") String code) {
-        return ResponseEntity.ok(employeeService.findByCode(code));
-    }
-
 }
